488eabe30293e6f2a01b5ca9f1d8db002e234048
koreoxy <rayssankn@gmail.com>
2023-10-02 23:41:18 +0700

    feat : siap di push

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-calculate = (event) => {
+const calculate = (event) => {
   event.preventDefault();
 
-  let berat = document.getElementById('berat').value;
-  let tinggi = document.getElementById('tinggi').value;
-  let hasil = document.getElementById('result');
-  let bmi = (berat / Math.pow(tinggi / 100, 2)).toFixed(1);
-  let form = document.getElementById('form');
+  const beratInput = document.getElementById('berat');
+  const tinggiInput = document.getElementById('tinggi');
+  const hasil = document.getElementById('result');
+  const form = document.getElementById('form');
 
-  if (berat === '' || isNaN(berat)) {
-    hasil.innerHTML = 'inputan tidak boleh kosong';
-  } else if (tinggi === '' || isNaN(tinggi)) {
-    hasil.innerHTML = 'inputan tidak boleh kosong';
+  const beratBadan = parseFloat(beratInput.value);
+  const tinggiBadan = parseFloat(tinggiInput.value);
+
+  if (
+    isNaN(beratBadan) ||
+    isNaN(tinggiBadan) ||
+    beratBadan <= 0 ||
+    tinggiBadan <= 0
+  ) {
+    hasil.innerHTML = `<span>Inputan berat dan tinggi harus valid dan lebih dari 0</span>`;
   } else {
-    if (bmi < 18.6) hasil.innerHTML = `Under Weight : <span>${bmi}</span>`;
-    else if (bmi >= 18.6 && bmi < 24.9)
-      hasil.innerHTML = `Normal : <span>${bmi}</span>`;
-    else hasil.innerHTML = `Over Weight : <span>${bmi}</span>`;
+    const bmi = (beratBadan / Math.pow(tinggiBadan / 100, 2)).toFixed(1);
+
+    if (bmi <= 18.5) {
+      hasil.innerHTML = `<span>Your BMI is <b>${bmi}</b> which means You are <b>Underweight</b></span>`;
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+      hasil.innerHTML = `<span>Your BMI is <b>${bmi}</b> which means You are <b>Normal</b></span>`;
+    } else if (bmi >= 25 && bmi < 29.9) {
+      hasil.innerHTML = `<span>Your BMI is <b>${bmi}</b> which means You are <b>Overweight</b></span>`;
+    } else {
+      hasil.innerHTML = `<span>Your BMI is <b>${bmi}</b> which means You are <b>Obesity</b></span>`;
+    }
   }
+
   form.reset();
 };
 
-let btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 btn.addEventListener('click', calculate);
